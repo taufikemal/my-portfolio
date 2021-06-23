d23dcda0a5a5e7ad64c3c44068939f188013a2c0
Taufik Kemal Thaha <artcrev7@gmail.com>
2021-06-23 21:31:11 +0800

    Adding illustrations

--- src/page/beranda.js
+++ src/page/beranda.js
@@ -1,35 +1,55 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import Layout from "../common/component/layout/layout";
-import { Box, Text, Container, Grid, GridItem, Image, Heading } from '@chakra-ui/react';
+import { Box, Text, Container, Grid, GridItem, Image, Heading, Wrap, WrapItem } from '@chakra-ui/react';
+import Pict1 from "../assets/images/pict 1.svg";
+import Pict2 from "../assets/images/pict 2.svg";
+import Pict3 from "../assets/images/pict 3.svg";
+import Pict4 from "../assets/images/pict 4.svg";
+import Pict5 from "../assets/images/pict 5.svg";
+import Pict6 from "../assets/images/pict 6.svg";
+import Pict7 from "../assets/images/pict 7.svg";
 
 export default function Beranda(props) {
 	return ( 
     <>
-			<Layout >
-        <Container maxW="8xl" paddingTop={{ base: "7", md: "20", lg: "40" }} paddingLeft={{ base: "7", md: "20", lg: "20" }} paddingRight={{ base: "7", md: "20", lg: "20" }} paddingBottom={{ base: "20" }} minHeight="90vh">
-				<Grid 
+		<Layout >
+        <Container 
+					maxW="8xl" 
+					pt ={{ base: "4", md: "20", lg: "20" }} 
+					pl ={{ base: "4", md: "20", lg: "10" }}
+					pr ={{ base: "4", md: "20", lg: "10" }} 
+					pb={{ base: "10" }} 
+					minHeight="90vh"
+				>
+				<Grid
 					templateColumns="repeat(2, 2fr)"
 					templateRows="repeat(2, 2fr)" 
 					gap={8}>
 					
-					<GridItem rowSpan={1}>
+					<GridItem 
+						width="full"
+						height="full"
+						alignContent="center"
+						mt={48} 
+						rowSpan={1}
+					>
 						<Box mt={{ base: "44", md: "28", lg: "0" }}>
 							<Fade duration={500} distance="30px" fraction={0.4} bottom>
 								<Heading 
 									as="h1"
 									bgGradient="linear(to-r, teal.500,green.500)"
 									bgClip="text"
-									fontSize="6xl"
+									fontSize="4xl"
 									fontWeight="extrabold"
 								>
 									Hello, My name is
 								</Heading>
 								<Heading 
 									as="h1"
-									bgGradient="linear(to-r, teal.500,green.500)"
+									bgGradient="linear(to-r, teal.500,green.400)"
 									bgClip="text"
-									fontSize="6xl"
+									fontSize="5xl"
 									fontWeight="extrabold"
 								>
 									Taufik Kemal Thaha
@@ -47,7 +67,7 @@ export default function Beranda(props) {
 									fontSize={{ base: "lg", md: "md", lg: "lg" }}
 								>
 								born in Palu, November 14, 2001. I like all about design, editing, videography, and music. Lives in Makassar and is currently studying at Software Engineering Technology Vocational School, Universitas Gadjah Mada, Yogyakarta, Indonesia.
-									</Text>
+								</Text>
 							</Fade>
 						</GridItem>
 					
@@ -55,12 +75,61 @@ export default function Beranda(props) {
 					<GridItem rowSpan={2} colSpan={1}>
 					<Box mt={{ base: "44", md: "28", lg: "0" }}>
         		<Fade duration={500} distance="30px" fraction={0.4} bottom>
-							<Image src="https://ik.imagekit.io/fmafj2zdejw/logo-2_1_vXQXqSgXw.png"/>
+							<Image 
+								ml="4"
+								width="full"
+								height="full"
+								src={Pict1}
+							/>
 						</Fade>
 					</Box>
 					</GridItem>
-
 				</Grid>	
+
+				
+				<Grid templateColumns="repeat(3, 1fr)" gap={6}>
+					<Box 
+						p={8}
+						w="100%" 
+						h="100%" 
+						bg="blue.100"
+					>
+						<Image 
+							width="full"
+							height="full"
+							src={Pict2}
+						/>
+					</Box>
+
+					<Box 
+						p={8}
+						w="100%" 
+						h="100%" 
+						bg="blue.100"
+					>
+						<Image 
+							width="full"
+							height="full"
+							src={Pict3}
+						/>
+					</Box>
+
+					<Box
+						boxShadow="lg"
+						rounded="md"
+						p={8}
+						w="100%" 
+						h="100%" 
+						bg="blue.100"
+					>
+						<Image 
+							width="full"
+							height="full"
+							src={Pict4}
+						/>
+					</Box>
+				
+				</Grid>
 				</Container>
 			</Layout>
 		</>
